701955b3e21239cff11648c8dc7b792663551d14
Fergyuson <magicklessons@gmail.com>
2025-09-25 19:02:31 +0300

    feat: create AccountTypeSelect component

---
+++ src/components/accounts/AccountTypeSelect.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: 'Локальная' | 'LDAP' | ''
+}
+
+interface Emits {
+  (e: 'update:modelValue', value: 'Локальная' | 'LDAP'): void
+  (e: 'change'): void
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const onChange = (value: 'Локальная' | 'LDAP') => {
+  emit('update:modelValue', value)
+  emit('change')
+}
+</script>
+
+<template>
+  <el-select
+      :model-value="modelValue"
+      @update:model-value="onChange"
+      placeholder="Выберите тип"
+  >
+    <el-option label="Локальная" value="Локальная" />
+    <el-option label="LDAP" value="LDAP" />
+  </el-select>
+</template>
